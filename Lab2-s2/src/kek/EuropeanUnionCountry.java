bc0581d62360096897d1af568fe0fed8acd70914
Fanncy <Fanncy@DESKTOP-KGFQSUC>
2019-04-11 02:25:34 +0300

    Ver 1.0

---
+++ Lab2-s2/src/kek/EuropeanUnionCountry.java
@@ -0,0 +1,81 @@
+package kek;
+
+public class EuropeanUnionCountry extends EuropeanCountry {
+
+    private int yearMember;
+
+    EuropeanUnionCountry(){//exemple de constructori
+        yearMember = 0;
+    }
+
+    EuropeanUnionCountry(String name, double population, String capital,
+                                String continent, String government, String leader,
+                                double area, String currency, int yearMember) {
+        super(name, population, capital, continent, government, leader, area, currency);
+        if (yearMember > 1957) {
+            this.yearMember = yearMember;
+        } else {
+            this.yearMember = 0;
+        }
+    }
+
+     EuropeanUnionCountry(String name, double population, double area, String currency, int yearMember) {
+        super(name, population, area, currency);
+        if (yearMember > 1957) {
+            this.yearMember = yearMember;
+        } else {
+            this.yearMember = 0;
+        }
+    }
+
+     EuropeanUnionCountry(String name, double area, String currency, int yearMember) {
+        super(name, area, currency);
+        if (yearMember > 1957) {
+            this.yearMember = yearMember;
+        } else {
+            this.yearMember = 0;
+        }
+    }
+
+    EuropeanUnionCountry(double area, String currency, int yearMember) {
+        super(area, currency);
+        if (yearMember > 1957) {
+            this.yearMember = yearMember;
+        } else {
+            this.yearMember = 0;
+        }
+    }
+
+    EuropeanUnionCountry(String name, int yearMember) {
+        super(name);
+        if (yearMember > 1957) {
+            this.yearMember = yearMember;
+        } else {
+            this.yearMember = 0;
+        }
+    }
+
+     EuropeanUnionCountry(int yearMember) {
+        if (yearMember > 1957) {
+            this.yearMember = yearMember;
+        } else {
+            this.yearMember = 0;
+        }
+    }
+
+    // setter
+    public void setYearMember(int yearMember) {
+        this.yearMember = yearMember;
+    }
+
+    // getter
+    public int getYearMember() {
+        return yearMember;
+    }
+
+    @Override
+    public void showDataOfCountry() {
+        super.showDataOfCountry();
+        System.out.print("Aderare la UE " + this.yearMember + '\n');
+    }
+}
---
+++ Lab2-s2/src/kek/Main.java
@@ -0,0 +1,45 @@
+package kek;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Country c = new Country("Rusia", 144.5);
+        c.showDataOfCountry();//ScreenShot0
+        System.out.println();
+
+        EuropeanUnionCountry uec = new EuropeanUnionCountry();//default
+        uec.showDataOfCountry();//ScreenShot1
+        System.out.println();
+
+        EuropeanCountry ec1 = new EuropeanCountry("Moldova", 3.35, "Chișinău",
+                "Europa", "Republică parlamentară", "Igor Dodon", 33.846, "MDL");
+        ec1.showDataOfCountry();//ScreenShot2
+        System.out.println();
+
+        EuropeanCountry ec2 = new EuropeanCountry("Austria", "Euro");
+        ec2.showDataOfCountry();//ScreenShot3
+        System.out.println();
+
+        EuropeanCountry ec3 = new EuropeanCountry("Ucraina", 603.628, "UAH");
+        ec3.showDataOfCountry();//ScreenShot4
+        System.out.println();
+
+        EuropeanUnionCountry euc1 = new EuropeanUnionCountry("Franța", 66.628, "Paris",
+                "Europa", "Republică semi-prezidențială", "Emmanuel Macron", 674.843, "Euro",1958);
+        euc1.showDataOfCountry();//ScreenShot5
+        System.out.println();
+
+        EuropeanUnionCountry euc2 = new EuropeanUnionCountry("România",19.64,238.397, "RON",2007);
+        euc2.showDataOfCountry();//ScreenShot6
+        System.out.println();
+
+        EuropeanUnionCountry euc3 = new EuropeanUnionCountry("Germania", 225.1, "EUR", 1958);
+        euc3.showDataOfCountry();//ScreenShot7
+        System.out.println();
+
+        AsianCountry asc1 = new AsianCountry("Japonia", 377.973, "Yen");
+        asc1.showDataOfCountry();//ScreenShot8
+
+    }
+}
