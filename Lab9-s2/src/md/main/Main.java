9f50e5d239619fc78525164bb08676311b48c2be
Fanncy <igorashsirbu@gmail.com>
2019-04-17 20:31:13 +0300

    Ver 0.02 after changes

--- Lab9-s2/src/md/main/Main.java
+++ Lab9-s2/src/md/main/Main.java
@@ -1,164 +1,164 @@
 package md.main;
 
 import md.file.FisierTari;
+
 import java.io.IOException;
 import java.util.Scanner;
+
 /**
- * 
  * @author Fanncy
- *
  */
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		Scanner read = new Scanner(System.in);
-		int oper;// oper for menu loop
-		Menu meniu = new Menu();// menu obj
-		String nameFile;
-		FisierTari tariFile = new FisierTari();
-		// menu loop :3
-		do {
-			meniu.clrscr();
-			tariFile.checkAssign();
-			oper = meniu.mainMenu();
-			switch (oper) {// main menu
+        Scanner read = new Scanner(System.in);
+        int oper;// oper for menu loop
+        Menu meniu = new Menu();// menu obj
+        String nameFile;
+        FisierTari tariFile = new FisierTari();
+        // menu loop :3
+        do {
+            meniu.clrscr();
+            tariFile.checkAssign();
+            oper = meniu.mainMenu();
+            switch (oper) {// main menu
 
-			case 1:// create new file
-				meniu.clrscr();
-				tariFile.checkAssign();
-				oper = meniu.opCreareFileMenu();
+                case 1:// create new file
+                    meniu.clrscr();
+                    tariFile.checkAssign();
+                    oper = meniu.opCreareFileMenu();
 
-				switch (oper) {
-				case 1:// for own path
-					meniu.clrscr();
-					tariFile.checkAssign();
-					meniu.opCreareFileMenuF1();
-					nameFile = read.next();
-					read.nextLine();
-					tariFile.creareFisier(nameFile);
-					break;
-				case 2:// for default path
-					meniu.clrscr();
-					tariFile.checkAssign();
-					meniu.opCreareFileMenuF2();
-					tariFile.creareFisier("countries");
-					break;
-				case 9:
-					meniu.clrscr();
-					break;
-				default:
-					if (oper != 0) {
-						meniu.errorOP(1);
-						meniu.pauseMenu();
-						meniu.clrscr();
-					}
-					break;
-				}
-				break;
-			case 2:// display all data from file
-				meniu.clrscr();
-				tariFile.checkAssign();
-				meniu.opAfisareMeniu();
-				tariFile.afisareFisier();
-				break;
-			case 3:// for adding a new country in file
-				meniu.clrscr();
-				tariFile.checkAssign();
-				meniu.opAddCountriesMenu();
+                    switch (oper) {
+                        case 1:// for own path
+                            meniu.clrscr();
+                            tariFile.checkAssign();
+                            meniu.opCreareFileMenuF1();
+                            nameFile = read.next();
+                            read.nextLine();
+                            tariFile.creareFisier(nameFile);
+                            break;
+                        case 2:// for default path
+                            meniu.clrscr();
+                            tariFile.checkAssign();
+                            meniu.opCreareFileMenuF2();
+                            tariFile.creareFisier("countries");
+                            break;
+                        case 9:
+                            meniu.clrscr();
+                            break;
+                        default:
+                            if (oper != 0) {
+                                meniu.errorOP(1);
+                                meniu.pauseMenu();
+                                meniu.clrscr();
+                            }
+                            break;
+                    }
+                    break;
+                case 2:// display all data from file
+                    meniu.clrscr();
+                    tariFile.checkAssign();
+                    meniu.opAfisareMeniu();
+                    tariFile.afisareFisier();
+                    break;
+                case 3:// for adding a new country in file
+                    meniu.clrscr();
+                    tariFile.checkAssign();
+                    meniu.opAddCountriesMenu();
 
-				if (tariFile.fileIsAssigned) {
-					do {
-						oper = meniu.opAddCountryMenu();
-						switch (oper) {
-						case 1:
-							tariFile.addDateInFisier();
-							break;
-						case 2:
-							break;
-						default:
-							if ((oper != 0) && (oper != 9)) {
-								meniu.errorOP(1);
-								meniu.pauseMenu();
-								meniu.clrscr();
-							}
-							break;
-						}
-					} while ((oper != 0) && (oper != 9));
-				} else {
-					meniu.errorOP(3);
-					meniu.pauseMenu();
-				}
-				break;
-			case 4:// display specific data from file
-				meniu.clrscr();
-				tariFile.checkAssign();
-				oper = meniu.opPrelucrareDate();
-				switch (oper) {
-				case 1:// display "Europa" countries from file
-					meniu.clrscr();
-					tariFile.afisareTariDinContinentul("Europa");
-					meniu.pauseMenu();
-					break;
-				case 2:// display "Asia" countries from file
-					meniu.clrscr();
-					tariFile.afisareTariDinContinentul("Asia");
-					meniu.pauseMenu();
-					break;
-				case 9:
-					meniu.clrscr();
-					break;
-				default:
-					if (oper != 0) {
-						meniu.errorOP(1);
-						meniu.pauseMenu();
-						meniu.clrscr();
-					}
-					break;
-				}
-				break;
-			case 5:// assign a new file or create a new one
-				meniu.clrscr();
-				tariFile.checkAssign();
-				nameFile = meniu.opAsociereFileMeniu();
-				tariFile.assignFile(nameFile);
-				break;
-			case 6:// deleting data from file
-				meniu.clrscr();
-				tariFile.checkAssign();
-				oper = meniu.opElimenareDateMenu();
+                    if (tariFile.fileIsAssigned) {
+                        do {
+                            oper = meniu.opAddCountryMenu();
+                            switch (oper) {
+                                case 1:
+                                    tariFile.addDateInFisier();
+                                    break;
+                                case 2:
+                                    break;
+                                default:
+                                    if ((oper != 0) && (oper != 9)) {
+                                        meniu.errorOP(1);
+                                        meniu.pauseMenu();
+                                        meniu.clrscr();
+                                    }
+                                    break;
+                            }
+                        } while ((oper != 0) && (oper != 9));
+                    } else {
+                        meniu.errorOP(3);
+                        meniu.pauseMenu();
+                    }
+                    break;
+                case 4:// display specific data from file
+                    meniu.clrscr();
+                    tariFile.checkAssign();
+                    oper = meniu.opPrelucrareDate();
+                    switch (oper) {
+                        case 1:// display "Europa" countries from file
+                            meniu.clrscr();
+                            tariFile.afisareTariDinContinentul("Europa");
+                            meniu.pauseMenu();
+                            break;
+                        case 2:// display "Asia" countries from file
+                            meniu.clrscr();
+                            tariFile.afisareTariDinContinentul("Asia");
+                            meniu.pauseMenu();
+                            break;
+                        case 9:
+                            meniu.clrscr();
+                            break;
+                        default:
+                            if (oper != 0) {
+                                meniu.errorOP(1);
+                                meniu.pauseMenu();
+                                meniu.clrscr();
+                            }
+                            break;
+                    }
+                    break;
+                case 5:// assign a new file or create a new one
+                    meniu.clrscr();
+                    tariFile.checkAssign();
+                    nameFile = meniu.opAsociereFileMeniu();
+                    tariFile.assignFile(nameFile);
+                    break;
+                case 6:// deleting data from file
+                    meniu.clrscr();
+                    tariFile.checkAssign();
+                    oper = meniu.opElimenareDateMenu();
 
-				switch (oper) {
-				case 1:// delete whole file
-					meniu.clrscr();
-					meniu.opStergereFisier();
-					tariFile.eliminareFisier();
-					break;
-				case 2:// delete specific country from file
-					meniu.clrscr();
-					meniu.opEliminareCountryMeniu();
-					tariFile.eliminareTara();
-					break;
-				case 9:
-					meniu.clrscr();
-					break;
-				default:
-					if (oper != 0) {
-						meniu.errorOP(1);
-						meniu.pauseMenu();
-						meniu.clrscr();
-					}
-				}
-				break;
-			default:
-				if (oper != 0) {
-					meniu.errorOP(1);
-					meniu.pauseMenu();
-					meniu.clrscr();
-				}
-				break;
-			}
-		} while (oper != 0);
-		read.close();
-	}
+                    switch (oper) {
+                        case 1:// delete whole file
+                            meniu.clrscr();
+                            meniu.opStergereFisier();
+                            tariFile.eliminareFisier();
+                            break;
+                        case 2:// delete specific country from file
+                            meniu.clrscr();
+                            meniu.opEliminareCountryMeniu();
+                            tariFile.eliminareTara();
+                            break;
+                        case 9:
+                            meniu.clrscr();
+                            break;
+                        default:
+                            if (oper != 0) {
+                                meniu.errorOP(1);
+                                meniu.pauseMenu();
+                                meniu.clrscr();
+                            }
+                    }
+                    break;
+                default:
+                    if (oper != 0) {
+                        meniu.errorOP(1);
+                        meniu.pauseMenu();
+                        meniu.clrscr();
+                    }
+                    break;
+            }
+        } while (oper != 0);
+        read.close();
+    }
 }
--- Lab9-s2/src/md/main/Menu.java
+++ Lab9-s2/src/md/main/Menu.java
@@ -4,158 +4,158 @@ import java.util.Scanner;
 
 /**
  * Used for making menu
- * @author Fanncy
  *
+ * @author Fanncy
  */
 public class Menu {
 
-	private Scanner read = new Scanner(System.in);
+    private Scanner read = new Scanner(System.in);
 
-	int mainMenu() {
+    int mainMenu() {
 
-		System.out.println("[================= Meniul Principal =================]");
-		System.out.println("Introduceți 1 Pentru crearea fișierului ");
-		System.out.println("Introduceți 2 Pentru afișarea conținutului fișierului");
-		System.out.println("Introduceți 3 Pentru adăugarea datelor în fișier");
-		System.out.println("Introduceți 4 Pentru prelucrarea  datelor din fișier");
-		System.out.println("Introduceți 5 pentru a asocia un nume de fișier");
-		System.out.println("Introduceți 6 pentru eliminarea datelor");
-		System.out.println("Introduceți 0 Pentru ieșirea din program");
-		System.out.println("------------------------------------------------------");
-		System.out.println(">Introduceți operația: ");
+        System.out.println("[================= Meniul Principal =================]");
+        System.out.println("Introduceți 1 Pentru crearea fișierului ");
+        System.out.println("Introduceți 2 Pentru afișarea conținutului fișierului");
+        System.out.println("Introduceți 3 Pentru adăugarea datelor în fișier");
+        System.out.println("Introduceți 4 Pentru prelucrarea  datelor din fișier");
+        System.out.println("Introduceți 5 pentru a asocia un nume de fișier");
+        System.out.println("Introduceți 6 pentru eliminarea datelor");
+        System.out.println("Introduceți 0 Pentru ieșirea din program");
+        System.out.println("------------------------------------------------------");
+        System.out.println(">Introduceți operația: ");
 
-		return Integer.parseInt(read.nextLine());
-	}
+        return Integer.parseInt(read.nextLine());
+    }
 
-	int opCreareFileMenu() {
+    int opCreareFileMenu() {
 
-		System.out.println("[================ Crearea fișierului ================]");
-		System.out.println("Introduceți 1 Pentru a crea un fișier cu un nume propriu");
-		System.out.println("Introduceți 2 Pentru a crea un fișier cu numele \"countries\"");
-		System.out.println("Introduceți 9 Pentru a reveni in meniul principal");
-		System.out.println("Introduceți 0 Pentru ieșirea din program");
-		System.out.println("------------------------------------------------------");
-		System.out.println(">Introduceți operația: ");
+        System.out.println("[================ Crearea fișierului ================]");
+        System.out.println("Introduceți 1 Pentru a crea un fișier cu un nume propriu");
+        System.out.println("Introduceți 2 Pentru a crea un fișier cu numele \"countries\"");
+        System.out.println("Introduceți 9 Pentru a reveni in meniul principal");
+        System.out.println("Introduceți 0 Pentru ieșirea din program");
+        System.out.println("------------------------------------------------------");
+        System.out.println(">Introduceți operația: ");
 
-		return Integer.parseInt(read.nextLine());
-	}
+        return Integer.parseInt(read.nextLine());
+    }
 
-	void opCreareFileMenuF1() {
+    void opCreareFileMenuF1() {
 
-		System.out.println("[=============== Crearea fișierului =================]");
-		System.out.println(">Introduceti numele fișierului");
-	}
+        System.out.println("[=============== Crearea fișierului =================]");
+        System.out.println(">Introduceti numele fișierului");
+    }
 
-	void opCreareFileMenuF2() {
+    void opCreareFileMenuF2() {
 
-		System.out.println("[=============== Crearea fișierului =================]");
-	}
+        System.out.println("[=============== Crearea fișierului =================]");
+    }
 
-	void opAddCountriesMenu() {
+    void opAddCountriesMenu() {
 
-		System.out.println("[=========== Adăugarea datelor în fișier ============]");
-	}
+        System.out.println("[=========== Adăugarea datelor în fișier ============]");
+    }
 
-	int opAddCountryMenu() {
+    int opAddCountryMenu() {
 
-		System.out.println("[========= [Adăugarea datelor despre Țară] ==========]");
-		System.out.println("Introduceți 1 Pentru a adăuga o Țară");
-		System.out.println("Introduceți 9 Pentru a reveni in meniul principal");
-		System.out.println("Introduceți 0 Pentru a ieși din program");
-		System.out.println("------------------------------------------------------");
-		System.out.println(">Introduceți operația: ");
+        System.out.println("[========= [Adăugarea datelor despre Țară] ==========]");
+        System.out.println("Introduceți 1 Pentru a adăuga o Țară");
+        System.out.println("Introduceți 9 Pentru a reveni in meniul principal");
+        System.out.println("Introduceți 0 Pentru a ieși din program");
+        System.out.println("------------------------------------------------------");
+        System.out.println(">Introduceți operația: ");
 
-		return Integer.parseInt(read.nextLine());
-	}
+        return Integer.parseInt(read.nextLine());
+    }
 
-	int opElimenareDateMenu() {
+    int opElimenareDateMenu() {
 
-		System.out.println("[================ Eliminarea datelor ================]");
-		System.out.println("Introduceți 1 Pentru a șterge fișierul");
-		System.out.println("Introduceți 2 Pentru a șterge o Țără");
-		System.out.println("Introduceți 9 Pentru a reveni in meniul principal");
-		System.out.println("Introduceți 0 Pentru a ieși din program");
-		System.out.println("------------------------------------------------------");
-		System.out.println(">Introduceți operația: ");
+        System.out.println("[================ Eliminarea datelor ================]");
+        System.out.println("Introduceți 1 Pentru a șterge fișierul");
+        System.out.println("Introduceți 2 Pentru a șterge o Țără");
+        System.out.println("Introduceți 9 Pentru a reveni in meniul principal");
+        System.out.println("Introduceți 0 Pentru a ieși din program");
+        System.out.println("------------------------------------------------------");
+        System.out.println(">Introduceți operația: ");
 
-		return Integer.parseInt(read.nextLine());
-	}
+        return Integer.parseInt(read.nextLine());
+    }
 
-	int opPrelucrareDate() {
+    int opPrelucrareDate() {
 
-		System.out.println("[========== Prelucrarea datelor din fișier ==========]");
-		System.out.println("Introduceți 1 Pentru a afișa țările din Europa");
-		System.out.println("Introduceți 2 Pentru a afișa țările din Asia");
-		System.out.println("Introduceți 9 Pentru a reveni in meniul principal");
-		System.out.println("Introduceți 0 Pentru a ieși din program");
-		System.out.println("------------------------------------------------------");
-		System.out.println(">Introduceți operația: ");
+        System.out.println("[========== Prelucrarea datelor din fișier ==========]");
+        System.out.println("Introduceți 1 Pentru a afișa țările din Europa");
+        System.out.println("Introduceți 2 Pentru a afișa țările din Asia");
+        System.out.println("Introduceți 9 Pentru a reveni in meniul principal");
+        System.out.println("Introduceți 0 Pentru a ieși din program");
+        System.out.println("------------------------------------------------------");
+        System.out.println(">Introduceți operația: ");
 
-		return Integer.parseInt(read.nextLine());
-	}
+        return Integer.parseInt(read.nextLine());
+    }
 
-	String opAsociereFileMeniu() {
+    String opAsociereFileMeniu() {
 
-		System.out.println("[=============== Asociera unui fisier ===============]");
-		System.out.println(">Introduceți numele fișierului");
+        System.out.println("[=============== Asociera unui fisier ===============]");
+        System.out.println(">Introduceți numele fișierului");
 
-		return read.nextLine();
-	}
+        return read.nextLine();
+    }
 
-	void opAfisareMeniu() {
+    void opAfisareMeniu() {
 
-		System.out.println("[========= Afișarea conținutului fișierului =========]");
-	}
+        System.out.println("[========= Afișarea conținutului fișierului =========]");
+    }
 
-	void opEliminareCountryMeniu() {
+    void opEliminareCountryMeniu() {
 
-		System.out.println("[============== Eliminarea unuei Țări ===============]");
-	}
+        System.out.println("[============== Eliminarea unuei Țări ===============]");
+    }
 
-	void opStergereFisier() {
-		System.out.println("[============== Ștergerea fișierului ================]");
-	}
+    void opStergereFisier() {
+        System.out.println("[============== Ștergerea fișierului ================]");
+    }
 
-	public void pauseMenu() {
+    public void pauseMenu() {
+        System.out.println("[----------------------------------------------------]");
+        System.out.println(">Introduceți 9 Pentru a continua!");
 
-		System.out.println("[----------------------------------------------------]");
-		System.out.println(">Introduceți 9 Pentru a continua!");
 
-		read.nextLine();
-	}
+        read.nextLine();
+    }
 
-	public void errorOP(int id) {
+    public void errorOP(int id) {
 
-		switch (id) {
-		case 1:
-			System.out.println("[----------------------------------------------------]");
-			System.out.println(" >Operație greșită! ");
-			break;
-		case 2:
-			System.out.println("[----------------------------------------------------]");
-			System.out.println(" >Fișierul nu a fost creat!");
-			break;
-		case 3:
-			System.out.println("[----------------------------------------------------]");
-			System.out.println(" >Eroare nu este asociat nici-un fișier!");
-			break;
-		case 4:
-			System.out.println("[----------------------------------------------------]");
-			System.out.println(" >Eroare nu s-a putut crea/asocia fișierul!");
-			break;
-		case 5:
-			System.out.println("[----------------------------------------------------]");
-			System.out.println(" >Eroare nu s-a putut afișa, nu este asociat nici-un fișier!");
-			break;
-		case 6:
-			System.out.println("[----------------------------------------------------]");
-			System.out.println(" >Eroare nu s-a putut introduce Țara!");
-		}
-	}
+        switch (id) {
+            case 1:
+                System.out.println("[----------------------------------------------------]");
+                System.out.println(" >Operație greșită! ");
+                break;
+            case 2:
+                System.out.println("[----------------------------------------------------]");
+                System.out.println(" >Fișierul nu a fost creat!");
+                break;
+            case 3:
+                System.out.println("[----------------------------------------------------]");
+                System.out.println(" >Eroare nu este asociat nici-un fișier!");
+                break;
+            case 4:
+                System.out.println("[----------------------------------------------------]");
+                System.out.println(" >Eroare nu s-a putut crea/asocia fișierul!");
+                break;
+            case 5:
+                System.out.println("[----------------------------------------------------]");
+                System.out.println(" >Eroare nu s-a putut afișa, nu este asociat nici-un fișier!");
+                break;
+            case 6:
+                System.out.println("[----------------------------------------------------]");
+                System.out.println(" >Eroare nu s-a putut introduce Țara!");
+        }
+    }
 
-	public void clrscr() {
-		for (int i = 0; i < 30; i++) {
-			System.out.println();
-		}
-	}
+    public void clrscr() {
+        for (int i = 0; i < 30; i++) {
+            System.out.println();
+        }
+    }
 }
