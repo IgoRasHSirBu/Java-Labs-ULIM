de3df1e3402019a5437af223ba37e89f26680c5f
Fanncy <igorashsirbu@gmail.com>
2019-04-19 19:33:48 +0300

    Ver. alpha-0.25 add RemoveCountryFrame

---
+++ Lab9-s2/src/md/views/RemoveCountryFrame.java
@@ -0,0 +1,45 @@
+package md.views;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class RemoveCountryFrame extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RemoveCountryFrame frame = new RemoveCountryFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public RemoveCountryFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Country DataBase: Remove Country");
+		setSize(600, 400);
+		setResizable(false);
+//		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	}
+
+}
