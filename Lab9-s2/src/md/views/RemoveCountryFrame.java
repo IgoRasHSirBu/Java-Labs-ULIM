5cae67f4772b87c1a0482c708b869c0dc521bf28
Fanncy <igorashsirbu@gmail.com>
2019-04-19 19:55:04 +0300

    Ver. alpha-0.25 add RemoveCountryFrame components

--- Lab9-s2/src/md/views/RemoveCountryFrame.java
+++ Lab9-s2/src/md/views/RemoveCountryFrame.java
@@ -6,10 +6,23 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JTextArea;
 
 public class RemoveCountryFrame extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textFieldNameToFind;
+	private JPanel panelMenu;
+	private JButton btnFind;
+	private JButton btnRemove;
+	private JLabel lblNameOfCountry;
+	private JPanel panelInfo;
+	private JTextArea txtrInformationAboutCountry;
 
 	/**
 	 * Launch the application.
@@ -19,7 +32,7 @@ public class RemoveCountryFrame extends JFrame {
 			public void run() {
 				try {
 					RemoveCountryFrame frame = new RemoveCountryFrame();
-					frame.setVisible(true);
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -27,19 +40,60 @@ public class RemoveCountryFrame extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public RemoveCountryFrame() {
+		init();
+	}
+
+	private void init() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Country DataBase: Remove Country");
 		setSize(600, 400);
 		setResizable(false);
-//		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-	}
 
+		panelMenu = new JPanel();
+		panelMenu.setBounds(300, 11, 284, 349);
+		contentPane.add(panelMenu);
+		panelMenu.setLayout(null);
+
+		textFieldNameToFind = new JTextField();
+		textFieldNameToFind.setFont(new Font("Tahoma", Font.BOLD, 16));
+		textFieldNameToFind.setBounds(10, 11, 264, 40);
+		panelMenu.add(textFieldNameToFind);
+		textFieldNameToFind.setColumns(10);
+
+		btnFind = new JButton("Find");
+		btnFind.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnFind.setBounds(10, 62, 132, 40);
+		panelMenu.add(btnFind);
+
+		btnRemove = new JButton("Remove");
+		btnRemove.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnRemove.setEnabled(false);
+		btnRemove.setBounds(142, 62, 132, 40);
+		panelMenu.add(btnRemove);
+
+		lblNameOfCountry = new JLabel("Name of Country:");
+		lblNameOfCountry.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNameOfCountry.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNameOfCountry.setBounds(10, 22, 284, 40);
+		contentPane.add(lblNameOfCountry);
+
+		panelInfo = new JPanel();
+		panelInfo.setBounds(10, 73, 284, 287);
+		contentPane.add(panelInfo);
+		panelInfo.setLayout(null);
+
+		txtrInformationAboutCountry = new JTextArea();
+		txtrInformationAboutCountry.setText("Information about Country");
+		txtrInformationAboutCountry.setFont(new Font("Arial Black", Font.BOLD, 11));
+		txtrInformationAboutCountry.setEditable(false);
+		txtrInformationAboutCountry.setBounds(10, 11, 264, 265);
+		panelInfo.add(txtrInformationAboutCountry);
+
+		setVisible(true);
+	}
 }
